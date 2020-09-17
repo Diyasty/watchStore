a66a405bb3cc6eb2cb9627ab0cbf77494d5f0d7a
Mohamed Eldiyasty <31715248+Diyasty@users.noreply.github.com>
2020-09-17 03:02:14 +0200

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -47,16 +47,7 @@ import { FavUserComponent } from "./dashboard/UserDashboard/FavUser/FavUser.comp
 import { AngularFireStorageModule, AngularFireStorage } from '@angular/fire/storage';
 
 // Bootstrap
-export const firebase = {
-  apiKey: "AIzaSyAcuLmYjW5uA3VggZ3NuakTBzScyz2oeMg",
-  authDomain: "maktabti-68c21.firebaseapp.com",
-  databaseURL: "https://maktabti-68c21.firebaseio.com",
-  projectId: "maktabti-68c21",
-  storageBucket: "maktabti-68c21.appspot.com",
-  messagingSenderId: "92395233535",
-  appId: "1:92395233535:web:7c96c41a76b28bd28cecca",
-  measurementId: "G-XJK8Z3DCGM",
-}
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -100,7 +91,7 @@ export const firebase = {
     ReactiveFormsModule,
     ToastrModule.forRoot(),
 
-    AngularFireModule.initializeApp(firebase),
+    AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule,
     AngularFireDatabaseModule,
   ],
