d961c368707873cfa1fafba310e031f1d61e7423
Mohamed Eldiyasty <31715248+Diyasty@users.noreply.github.com>
2020-09-17 03:00:09 +0200

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,14 +5,14 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyAcuLmYjW5uA3VggZ3NuakTBzScyz2oeMg",
-    authDomain: "maktabti-68c21.firebaseapp.com",
-    databaseURL: "https://maktabti-68c21.firebaseio.com",
-    projectId: "maktabti-68c21",
-    storageBucket: "maktabti-68c21.appspot.com",
-    messagingSenderId: "92395233535",
-    appId: "1:92395233535:web:7c96c41a76b28bd28cecca",
-    measurementId: "G-XJK8Z3DCGM",
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: "",
   },
 };
 
